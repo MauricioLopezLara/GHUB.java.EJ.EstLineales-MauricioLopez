e0dcef99a70ea07b4c43c79c2f8a8e5223173382
armenta lindoro maria lourdes <137097961+mariaLourdesArmenta@users.noreply.github.com>
2024-03-20 16:37:15 +0000

    Actualizacion cola de vuelos

---
+++ miPrincipal/SimuladorVuelos.java
@@ -0,0 +1,11 @@
+package miPrincipal;
+public class SimuladorVuelos {
+    public static void simular(){
+        Cola<Vuelo> cola = new Cola<Vuelo>();
+        while (!cola.esVacia()){
+            
+        }
+    }
+
+    
+}
---
+++ miPrincipal/Vuelo.java
@@ -0,0 +1,9 @@
+package miPrincipal;
+public class Vuelo{
+    public void leerVuelosDesdeArchivo(){
+
+    }
+    public boolean hayAterrizaje(){
+        return true;
+    }
+}
